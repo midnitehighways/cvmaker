29e26dcde554b9c3789714a0a90246561de3080e
Alex <alexandru.oat@gmail.com>
2016-04-02 18:21:16 +0300

    Add form. Handle basic post request

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 // app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+var counter=0;
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Build your CV' });
+  res.render('index', { title: 'Build your CV', counter: counter });
 });
+
+router.post('/add', function (req, res) {
+  counter++;
+  console.log(req.body.fn + " " + req.body.sn);
+  res.redirect('/');
+});
+
 
 module.exports = router;
