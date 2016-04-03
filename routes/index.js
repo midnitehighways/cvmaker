6742008c51d30cf3dcd19c8a77d8c412c44c1fe7
Alex <alexandru.oat@gmail.com>
2016-04-03 21:30:22 +0300

    Place pdf model on page

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+// TO DO 
+// MAKE CV OBJECT with name, edu and other fields
+//
 /* GET home page. */
-var counter=0;
+var fn="anon";
+var ln="anon";
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Build your CV', counter: counter });
+  res.render('index', { title: 'Build your CV', fn:fn, ln:ln, });
 });
 
 /* handling POST request from form */
 router.post('/add', function (req, res) {
-  counter++;
-  console.log(req.body.fn + " " + req.body.sn);
+  fn = req.body.fn;
+  ln = req.body.ln;
+  console.log(req.body.fn + " " + req.body.ln);
   res.redirect('/');
 });
 
