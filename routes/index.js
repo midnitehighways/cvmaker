1b5824f188f56db60573e80d9e8b607b39e1448f
Alex <alexandru.oat@gmail.com>
2016-04-04 11:29:09 +0300

    Add additional functionality for jsPDF

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,32 @@
 var express = require('express');
 var router = express.Router();
-// TO DO 
-// MAKE CV OBJECT with name, edu and other fields
-//
+/* TO DO 
+ MAKE CV OBJECT with name, edu and other fields
+ CHANGE LANGUAGE */
 /* GET home page. */
-var fn="anon";
-var ln="anon";
+var person = {
+	fullName : "Alexandru Oat",
+	email : "oat.alexandru@gmail.com",
+	phone : "+358466360623",
+	// gender = 
+	born : "23.03.1982",  
+	address : "",
+	education : []
+}
+var full_name="Alexandru Oat";
+
+var ln = "";
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Build your CV', fn:fn, ln:ln, });
+  res.render('index', { title: 'Build your CV', full_name:person.fullName, person:person, ln:ln, });
 });
 
 /* handling POST request from form */
 router.post('/add', function (req, res) {
-  fn = req.body.fn;
-  ln = req.body.ln;
-  console.log(req.body.fn + " " + req.body.ln);
+  person.fullName = req.body.full_name;
+  person.email = req.body.email;
+  person.phone = req.body.phone;
+  person.address = req.body.address;
+  // console.log(req.body.full_name + " " + req.body.ln);
   res.redirect('/');
 });
 
