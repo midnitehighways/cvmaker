ec968ec8854003f54b02cb84ccc137492fb4d49f
Alex <alexandru.oat@gmail.com>
2016-04-14 23:17:19 +0300

    Add AngularJS tab-menu. Remove PersonCtrl for now

--- public/javascripts/controllers/controllers.js
+++ public/javascripts/controllers/controllers.js
@@ -1,37 +1,36 @@
-cvApp.controller("MenuCtrl", function() {
-    this.tab = 2;
-    console.log("chose");
+var cvApp = angular.module('cvApp', []);
+        cvApp.controller("MenuCtrl", function() {
+            this.tab = 1;                               // tab shown by default
 
-    this.isChosen = function(checkTab) {
-      return this.tab === checkTab;
-    };
-
-    this.chooseTab = function(chooseTab) {
-      this.tab = setTab;
-    };
-});
-
-
-cvApp.controller('PersonCtrl', function(){
-                this.p = person;
-        });
-            var person = {
-                fullName: "fullName || ",
-                email: "a",
-                phone: "333",
-                born: "23.03.82",
-                address: "Rälssintie 16 B 14 Helsinki",
-                citizenship: "MM",
-                education: [{university: "Haaga-Helia University of Applied Sciences",
-                faculty: "Business Information Technology",
-                from: "Aug. 2013",
-                till: "Dec. 2016"}],
-                employment: [{university: "Haaga-Helia University of Applied Sciences",
-                faculty: "Business Information Technology",
-                from: "Aug. 2013",
-                till: "Dec. 2016"}],
-                skills: ["some skills"],
-                languages: ["suomi"],
-                about: "about",
-                pic: ""
+            this.isSet = function(checkTab) {
+              return this.tab === checkTab;
             };
+
+            this.setTab = function(setTab) {
+              this.tab = setTab;
+            };
+        });
+
+// cvApp.controller('PersonCtrl', function(){
+//                 this.p = person;
+//         });
+//             var person = {
+//                 fullName: "fullName || ",
+//                 email: "a",
+//                 phone: "333",
+//                 born: "23.03.82",
+//                 address: "Rälssintie 16 B 14 Helsinki",
+//                 citizenship: "MM",
+//                 education: [{university: "Haaga-Helia University of Applied Sciences",
+//                 faculty: "Business Information Technology",
+//                 from: "Aug. 2013",
+//                 till: "Dec. 2016"}],
+//                 employment: [{university: "Haaga-Helia University of Applied Sciences",
+//                 faculty: "Business Information Technology",
+//                 from: "Aug. 2013",
+//                 till: "Dec. 2016"}],
+//                 skills: ["some skills"],
+//                 languages: ["suomi"],
+//                 about: "about",
+//                 pic: ""
+//             };
